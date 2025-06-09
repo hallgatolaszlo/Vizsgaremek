914a97891ee7ec67fc1a7cbaff012ddac229dfbf
Laszlo Hallgato <hallgato.laszlo1124@gmail.com>
2025-06-09 18:14:39 +0200

    got rid of leftover localstorage code for tokens

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
@@ -9,18 +8,5 @@ const api = axios.create({
         Accept: "application/json",
     },
 });
-
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token) {
-            config.headers.Authorization = "Bearer ${token}";
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
 
 export default api;
