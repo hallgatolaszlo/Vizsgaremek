6d72778b6100a21f761c3ae9fdbcd81557e63fcd
hallgato.laszlo <hallgato.laszlo1124@gmail.com>
2025-06-12 11:57:51 +0200

    removed wait-for-it.sh files and changed root color

--- frontend/src/pages/DailyJournalPage.tsx
+++ frontend/src/pages/DailyJournalPage.tsx
@@ -1,3 +1,6 @@
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { type JSX } from "react";
 import { useForm } from "react-hook-form";
 import MoodSlider from "../components/daily-journal/MoodSlider.tsx";
@@ -39,38 +42,42 @@ function DailyJournalPage(): JSX.Element {
     }
 
     return (
-        <div className="p-[25px] flex justify-center items-center bg-color-1 min-h-screen">
-            <div
-                style={{ border: "3px solid var(--btn-color-1)" }}
-                className="p-[25px] gap-7 flex flex-col items-center rounded-[20px] bg-color-2 w-full h-fit max-w-[500px] login-card"
-            >
-                <h1 className="font-semibold text-center leading-[100%] title-font text-color-1 text-[46px]">
-                    Daily Journal
-                </h1>
-                <div className="w-full">
-                    <Hr />
-                </div>
-                <form
-                    onSubmit={handleSubmit(onSubmit)}
-                    className="flex items-center flex-col gap-[30px] max-w-[350px] w-full"
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <div className="p-[25px] flex flex-col justify-center items-center bg-color-1 min-h-screen">
+                <DateCalendar className="bg-color-2 rounded" />
+
+                <div
+                    style={{ border: "3px solid var(--btn-color-1)" }}
+                    className="p-[25px] gap-7 flex flex-col items-center rounded-[20px] bg-color-2 w-full h-fit max-w-[500px] login-card"
                 >
-                    <div className="w-full flex flex-col gap-[10px]">
-                        <MoodSlider register={register("mood")} />
+                    <h1 className="font-semibold text-center leading-[100%] title-font text-color-1 text-[46px]">
+                        Daily Journal
+                    </h1>
+                    <div className="w-full">
+                        <Hr />
                     </div>
-                    <textarea
-                        className="w-full min-h-[100px] text-font p-[10px] bg-[var(--soft-white)]"
-                        placeholder="How was your day?"
-                        {...register("journal_entry")}
-                    ></textarea>
-                    <div className="w-full flex justify-center">
-                        <SubmitButton content="Submit"></SubmitButton>
-                    </div>
-                    <button type="button" onClick={getDailyJournals}>
-                        get
-                    </button>
-                </form>
+                    <form
+                        onSubmit={handleSubmit(onSubmit)}
+                        className="flex items-center flex-col gap-[30px] max-w-[350px] w-full"
+                    >
+                        <div className="w-full flex flex-col gap-[10px]">
+                            <MoodSlider register={register("mood")} />
+                        </div>
+                        <textarea
+                            className="w-full min-h-[100px] text-font p-[10px] bg-[var(--soft-white)]"
+                            placeholder="How was your day?"
+                            {...register("journal_entry")}
+                        ></textarea>
+                        <div className="w-full flex justify-center">
+                            <SubmitButton content="Submit"></SubmitButton>
+                        </div>
+                        <button type="button" onClick={getDailyJournals}>
+                            Get
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </LocalizationProvider>
     );
 }
 
