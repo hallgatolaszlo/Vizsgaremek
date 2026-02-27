ac351fcc90a8b8689844c002e9d8763b8fa11d99
csudi <hallgato.laszlo1124@gmail.com>
2026-02-27 10:46:42 +0100

    fixed problems and switched to webpack

--- packages/features/index.tsx
+++ packages/features/index.tsx
@@ -1,5 +1,4 @@
 export * from "./src/auth/AuthCard";
-export * from "./src/auth/ProtectedRoute";
 export * from "./src/auth/SignInForm";
 export * from "./src/auth/SignUpForm";
 export * from "./src/calendar/Calendar";
